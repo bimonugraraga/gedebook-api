b9bfab10d55e9f54162dfa573d58a82be0da248f
ilhamsenaaa <ilhamsenaa@gmail.com>
2023-01-11 10:59:18 +0700

    [ilhamsena] update go work go mod go sum, database

--- http/db/database.go
+++ http/db/database.go
@@ -17,7 +17,7 @@ var once sync.Once
 
 func InitDB() {
 	once.Do(func() {
-		dsn := "postgres://postgres:postgres@localhost:5432/db_gedebook?sslmode=disable"
+		dsn := "postgres://postgres:Scarlet13@localhost:5432/db_gedebook?sslmode=disable"
 		sqlDb := sql.OpenDB(pgdriver.NewConnector(pgdriver.WithDSN(dsn)))
 		dbInstance = bun.NewDB(sqlDb, pgdialect.New())
 		// InitLogger(dbInstance, c.Debug, c.DebugLevel)
